19d8ea6d4ce5aceff97ab0a7dcd6624cfcb7f76d
RichForever20 <ayomideolabode26@icloud.com>
2024-07-05 07:33:37 +0100

    first commit

---
+++ font.js
@@ -0,0 +1,12 @@
+// script.js
+
+// Select the anchor tag by its ID
+var anchor = document.getElementById('myAnchor');
+
+// Apply styles using JavaScript
+anchor.style.Color = 'black'; // Change text color to red
+anchor.style.fontSize = ''; // Change font size to 20px
+anchor.style.textDecoration = ''; // Underline the text
+
+// You can also add a class to the anchor tag if you prefer to define styles in CSS
+anchor.classList.add('styled-anchor');
